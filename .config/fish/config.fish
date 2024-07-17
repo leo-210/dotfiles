6fb88c4c0019b6c0e3f2b9493dcf8be6366bb40e
Léo Kosman <leo.kosman@proton.me>
2024-07-17 22:24:16 +0200

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+fish_add_path ~/.local/bin
+
+zoxide init --cmd cd fish | source
+
+thefuck --alias | source
+
+# opam configuration
+source /home/leo/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+fish_add_path /home/leo/.spicetify
