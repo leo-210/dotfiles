f70702c5e59a307ebbc0acc6cef02e8b0ba9ef81
Léo Kosman <leo.kosman@proton.me>
2024-09-06 16:15:50 +0200

    made chages a long time ago

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ end
 set -g fish_greeting
 
 fish_add_path ~/.local/bin
+fish_add_path ~/.cache/rebar3/bin
+
 
 zoxide init --cmd cd fish | source
 
@@ -13,5 +15,4 @@ thefuck --alias | source
 # opam configuration
 source /home/leo/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
-fish_add_path /home/leo/.spicetify
 abbr --add dotfiles "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
